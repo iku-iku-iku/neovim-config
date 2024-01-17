fd413ad55f2846037b78c9c0b6c604db981935a3
wyl <2548340423@qq.com>
2024-01-17 23:04:46 +0800

    update dap

--- init.lua
+++ init.lua
@@ -17,3 +17,37 @@ end
 -- vim.cmd("colorscheme tokyonight-moon") -- for moon theme
 -- vim.cmd("colorscheme tokyonight-night") -- for night themes
 require("onedark").load()
+
+local dap = require("dap")
+EXTENSION_PATH = "/home/code4love/PROGRAM/LINUX/MyLinux-0.11/Linux-0.11/debug/extension"
+dap.adapters.cppdbg = {
+  id = "cppdbg",
+  type = "executable",
+  command = EXTENSION_PATH .. "/debugAdapters/bin/OpenDebugAD7",
+}
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtEntry = true,
+  },
+  {
+    name = "Attach to gdbserver :1234",
+    type = "cppdbg",
+    request = "launch",
+    MIMode = "gdb",
+    miDebuggerServerAddress = "localhost:1234",
+    miDebuggerPath = "/usr/bin/gdb",
+    cwd = "${workspaceFolder}",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+  },
+}
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
