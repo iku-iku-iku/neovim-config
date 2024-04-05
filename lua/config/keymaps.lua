2419aed955fc1ec70de5c6e8dbb39880ee1fd96c
Yilong Wang <2548340423@qq.com>
2024-04-05 17:35:41 +0800

    add live_grep_args

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,19 +9,17 @@ local Util = require("lazyvim.util")
 local map = Util.safe_keymap_set
 -- quit file
 map({ "i", "x", "n", "s" }, "<C-q>", "<cmd>q<cr><esc>", { desc = "Quit file" })
--- end the line with ';'
-map({ "i", "n" }, "<C-e>", "<esc>A;<esc>", { desc = "End line with ';'" })
 for i = 1, 9 do
-  map({ "n" }, "g" .. i, "<cmd>BufferLineGoToBuffer " .. i .. "<cr><esc>", { desc = "goto buffer " .. i })
+    map({ "n" }, "g" .. i, "<cmd>BufferLineGoToBuffer " .. i .. "<cr><esc>", { desc = "goto buffer " .. i })
 end
 
 map({ "n" }, "<C-q>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "harpoon ui" })
 map({ "n" }, "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "harpoon add current file" })
 vim.api.nvim_set_keymap(
-  "n",
-  "<C-w>",
-  "<cmd>lua require('telescope.builtin').live_grep({ default_text = vim.fn.expand('<cword>') })<CR>",
-  { noremap = true, silent = true, desc = "Live Grep Current Word" }
+    "n",
+    "<C-w>",
+    "<cmd>lua require('telescope.builtin').live_grep({ default_text = vim.fn.expand('<cword>') })<CR>",
+    { noremap = true, silent = true, desc = "Live Grep Current Word" }
 )
 
 -- 定义一个Lua函数来实现live_grep当前光标下的单词
@@ -39,36 +37,40 @@ vim.api.nvim_set_keymap(
 -- )
 
 vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
+    "n",
+    "<leader>sx",
+    require("telescope.builtin").resume,
+    { noremap = true, silent = true, desc = "Resume" }
 )
 
 function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+    local opts = { buffer = 0 }
+    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 end
 
 function MOVE_SCREEN_UP()
-  for _ = 1, 10 do
-    vim.api.nvim_input("k<C-y>")
-  end
+    for _ = 1, 10 do
+        vim.api.nvim_input("k<C-y>")
+    end
 end
 
 function MOVE_SCREEN_DOWN()
-  for _ = 1, 10 do
-    vim.api.nvim_input("j<C-e>")
-  end
+    for _ = 1, 10 do
+        vim.api.nvim_input("j<C-e>")
+    end
 end
+
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 map("n", "<leader>cf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 map("n", "<Up>", "k<C-y>", opts)
 map("n", "<Down>", "j<C-e>", opts)
 map("n", "<Left>", "<cmd>lua MOVE_SCREEN_UP()<CR>", { silent = true })
+map("n", "<Right>", "<cmd>lua MOVE_SCREEN_DOWN()<CR>", { silent = true })
+
+map("n", "<leader>j", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
